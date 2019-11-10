dfab019f016b34ff547d3c5a67101491815ac758
Rahul Ghangas <rahulghangas0@gmail.com>
2019-11-11 09:06:58 +1100

    Bug fix to velocity vector

--- CFD_demo.pyx
+++ CFD_demo.pyx
@@ -2,63 +2,73 @@ import numpy as np
 import pygame
 import cython
 import random
-import colorsys
+import math
+from noise import pnoise1
+from pygame.locals import *
 
-cdef int N = 128
-cdef int iterations = 16
-
-cdef inline int IX(int x_coord, int y_coords):
-        return x_coord + y_coords*N
-
-cdef inline int denorm(tup):
-    return (tup[0]*255, tup[1]*255, tup[2]*255)
+cdef int N = 256
+cdef int iterations = 4
+cdef int scale = 2
+cdef float t = 0
 
 cdef float visc, diff, dt, x , y, d, vx, vy, angle
 cdef float [:] Vx, Vy, Vx0, Vy0, s, density
-cdef int i, j, k, cx, cy
-cdef int scale = 4
-
+cdef int i, j, k, cx, cy, index
 cdef float i0, i1, j0, j1, dtx, dty, s0, s1, t0, t1, tmp1, tmp2, Nfloat, ifloat, jfloat
+cdef float cRecip
+
+cdef inline int IX(int x_coord, int y_coord):
+        return x_coord + y_coord*N
 
 
-class Fluid():
+cdef class Fluid:
+    cdef int size
+    cdef float dt
+    cdef float diff
+    cdef float visc
+    cdef float [:] s
+    cdef float [:] density
+    cdef float [:] Vx
+    cdef float [:] Vy
+    cdef float [:] Vx0
+    cdef float [:] Vy0
+
     def __init__(self, dt, diffusion, viscosity):
-        global N
         self.size = N
         self.dt = dt
         self.diff = diffusion
         self.visc = viscosity
 
-        self.s = np.zeros((self.size*self.size), dtype=np.float32)
-        self.density = np.zeros((self.size*self.size), dtype=np.float32)
+        self.s = np.zeros(N*N, dtype=np.float32)
+        self.density = np.zeros(N*N, dtype=np.float32)
 
 
-        self.Vx = np.zeros((self.size*self.size), dtype=np.float32)
-        self.Vy = np.zeros((self.size*self.size), dtype=np.float32)
+        self.Vx = np.zeros(N*N, dtype=np.float32)
+        self.Vy = np.zeros(N*N, dtype=np.float32)
 
-        self.Vx0 = np.zeros((self.size*self.size), dtype=np.float32)
-        self.Vy0 = np.zeros((self.size*self.size), dtype=np.float32)
+        self.Vx0 = np.zeros(N*N, dtype=np.float32)
+        self.Vy0 = np.zeros(N*N, dtype=np.float32)
 
 
     def addDensity(self, int x_coord, int y_coord, float amount):
         self.density[IX(x_coord, y_coord)] += amount
 
     def addVelocity(self, int x_coord, int y_coord, float addVelX, float addVelY):
-        cdef index = IX(x_coord, y_coord)
+        index = IX(x_coord, y_coord)
 
         self.Vx[index] += addVelX
         self.Vy[index] += addVelY
 
     def step(self):
-        visc     = self.visc;
-        diff     = self.diff;
-        dt       = self.dt;
-        Vx      = self.Vx;
-        Vy      = self.Vy;
-        Vx0     = self.Vx0;
-        Vy0     = self.Vy0;
-        s       = self.s;
-        density = self.density;
+        visc     = self.visc
+        diff     = self.diff
+        dt       = self.dt
+        Vx      = self.Vx
+        Vy      = self.Vy
+        Vx0     = self.Vx0
+        Vy0     = self.Vy0
+        s       = self.s
+        density = self.density
 
         diffuse(1, Vx0, Vx, visc, dt)
         diffuse(2, Vy0, Vy, visc, dt)
@@ -79,8 +89,8 @@ class Fluid():
             for j in range(N):
                 x = i * scale
                 y = j * scale
-                d = self.density[IX(i, j)]
-                pygame.draw.rect(screen, denorm(colorsys.hsv_to_rgb(((d + 50) % 360) / 360,200 / 200, np.clip(d// 3, 0, 100)/ 100)), (x, y, scale, scale))
+                d = self.density[IX(i, j)] % 255
+                pygame.draw.rect(screen, (d, d, d), (x, y, scale, scale))
 
     def renderV(self, screen):
 
@@ -88,15 +98,16 @@ class Fluid():
             for j in range(N):
                 x = i * scale
                 y = j * scale
-                vx = self.Vx[IX(i, j)];
-                vy = self.Vy[IX(i, j)];
+                vx = self.Vx[IX(i, j)]
+                vy = self.Vy[IX(i, j)]
 
                 if (not (abs(vx) < 0.1 and abs(vy) <= 0.1)):
-                    pygame.draw.line(screen, x, y, x+vx*scale, y+vy*scale, 1)
+                    pygame.draw.line(screen, (0, 0, 0), (x, y), (x+vx*scale, y+vy*scale))
 
     def fadeD(self, screen):
         for i in range(len(self.density)):
             d = self.density[i]
+            
             self.density[i] = np.clip(d-0.02, 0, 255)
 
 def diffuse (int b, float [:] x_array, float [:] x0_array, float diff, float dt):
@@ -105,7 +116,7 @@ def diffuse (int b, float [:] x_array, float [:] x0_array, float diff, float dt)
 
 
 def lin_solve(int b, float [:] x_array, float [:] x0_array, float a, float c):
-    cdef float cRecip = 1.0 / c
+    cRecip = 1.0 / c
     for k in range(iterations):
         for j in range(1, N-1):
             for i in range(1, N-1):
@@ -132,8 +143,8 @@ def project(float [:] velocX_array, float [:] velocY_array, float [:] p_array, f
             p_array[IX(i, j)] = 0
 
 
-    set_bnd(0, div_array);
-    set_bnd(0, p_array);
+    set_bnd(0, div_array)
+    set_bnd(0, p_array)
     lin_solve(0, p_array, div_array, 1, 6)
 
 
@@ -162,17 +173,17 @@ def advect(int b, float [:] d_array, float [:] d0_array,  float [:] velocX_array
 
             if (x < 0.5): x = 0.5
             if(x > Nfloat + 0.5): x = Nfloat + 0.5
-            i0 = np.floor(x)
+            i0 = math.floor(x)
             i1 = i0 + 1.0
             if(y < 0.5): y = 0.5
             if(y > Nfloat + 0.5): y = Nfloat + 0.5
-            j0 = np.floor(y)
+            j0 = math.floor(y)
             j1 = j0 + 1.0
 
-            s1 = x - i0;
-            s0 = 1.0 - s1;
-            t1 = y - j0;
-            t0 = 1.0 - t1;
+            s1 = x - i0
+            s0 = 1.0 - s1
+            t1 = y - j0
+            t0 = 1.0 - t1
 
             i0i = int(i0)
             i1i = int(i1)
@@ -199,36 +210,37 @@ def set_bnd(int b, float [:] x_array):
     x_array[IX(N-1, N-1)] = 0.5 * (x_array[IX(N-2, N-1)] + x_array[IX(N-1, N-2)])
 
 
+cdef int width = N*scale
+cdef int height = N*scale
 pygame.init()
-screen = pygame.display.set_mode((N * scale, N * scale))
+flags = DOUBLEBUF
+screen = pygame.display.set_mode(size=(width, height), flags=flags)
+screen.set_alpha(None)
 done = False
 
-fluid = Fluid(0.2, 0, 0.0000001)
-
-color = 0
-colors = [(255, 255, 255), (0,0,0)]
+fluid = Fluid(1, 0, 0.0000001)
 
 while not done:
-    screen.fill((0,0,0))
-    cx = int(0.5*N)
-    cy = int(0.5*N)
+    cx = int(0.5*width/scale)
+    cy = int(0.5*height/scale)
     for i in range(-1, 2):
         for j in range(-1, 2):
-            fluid.addDensity(cx+i, cy+j, random.randint(50, 150))
+            fluid.addDensity(cx+ i, cy+j, random.randint(50, 150))
 
-    for i in range(-1, 2):
-        angle = (random.random() * 2 * np.pi) - np.pi
-        fluid.addVelocity(cx, cy, np.cos(angle) * 0.2, np.cos(angle) * 0.2)
+    for i in range(2):
+        angle = (pnoise1(t, 1) * 2 * math.pi)
+        t += 0.01
+        fluid.addVelocity(cx, cy, math.cos(angle) * 0.2, math.sin(angle) * 0.2)
 
 
     fluid.step()
     fluid.renderD(screen)
     # fluid.renderV(screen)
     # fluid.fadeD(screen)
+    pygame.display.update()
 
     for event in pygame.event.get():
             if event.type == pygame.QUIT:
                     done = True
-
-    pygame.display.flip()
+    
 pygame.quit()
